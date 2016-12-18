32001b6a9d8c4b1c7bb8f502d4814dc05e655388
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2016-12-18 19:05:04 +0100

    Update to plotly.js version 1.21.2

--- lib/Chart/Plotly.pm
+++ lib/Chart/Plotly.pm
@@ -120,7 +120,7 @@ sub _render_cell {
     }	
     my $template    = <<'TEMPLATE';
 <div id="{$chart_id}"></div>
-<script src="https://cdn.plot.ly/plotly-1.20.5.min.js"></script>
+<script src="https://cdn.plot.ly/plotly-1.21.2.min.js"></script>
 <script>
 Plotly.plot(document.getElementById('{$chart_id}'),{$data} {$layout});
 </script>
