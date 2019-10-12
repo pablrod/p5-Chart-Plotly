48ed70d5aa720f2dee6a4b9ed943e33f7ab0d648
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2019-10-12 14:52:55 +0200

    Generate images and html from all examples

--- tool/generate_images_from_examples.pl
+++ tool/generate_images_from_examples.pl
@@ -8,30 +8,35 @@ use Path::Tiny;
 
 my $driver = Selenium::Firefox->new( marionette_enabled => 1, firefox_binary => "/opt/firefox/firefox-bin" );
 
-$driver->set_window_size(520, 480);
-my $samples_folder = path('examples/traces');
-for my $trace_example ($samples_folder->children(qr/\.pl$/)) {
-    print "Trace file $trace_example\n";
-    
-    my $trace_name = $trace_example->basename;
-    $trace_name =~ s/\.pl$//;
-    
-    system('perl -Ilib ' . $trace_example);
-    
-    my $tmp_path = path('/tmp');
-    my @candidate_files = sort {(stat($b))[9] <=> (stat($a))[9]} $tmp_path->children(qr/\.html$/);
-    print $candidate_files[0], "\n";
-    
-    $driver->get('file://' . $candidate_files[0]);
-    my $image_name = $trace_name . ".png";
-    sleep(30);
-    $driver->capture_screenshot($samples_folder->child($image_name) . "");
+$driver->set_window_size( 520, 480 );
 
-    my $html_trace_file = $samples_folder->child($trace_name . ".html");
-    $candidate_files[0]->copy($html_trace_file);
+my @paths = ( 'examples/traces', 'examples' );
+
+for my $path (@paths) {
+    my $samples_folder = path($path);
+    for my $trace_example ( $samples_folder->children(qr/\.pl$/) ) {
+        print "Trace file $trace_example\n";
+
+        my $trace_name = $trace_example->basename;
+        $trace_name =~ s/\.pl$//;
+
+        system( 'perl -Ilib ' . $trace_example );
+
+        my $tmp_path = path('/tmp');
+        my @candidate_files = sort { ( stat($b) )[9] <=> ( stat($a) )[9] } $tmp_path->children(qr/\.html$/);
+        print $candidate_files[0], "\n";
+
+        $driver->get( 'file://' . $candidate_files[0] );
+        my $image_name = $trace_name . ".png";
+        sleep(30);
+        $driver->capture_screenshot( $samples_folder->child($image_name) . "" );
+
+        my $html_trace_file = $samples_folder->child( $trace_name . ".html" );
+        $candidate_files[0]->copy($html_trace_file);
+    }
 }
 
 $driver->quit;
 
-system('montage ' . $samples_folder . '/*.png examples/montage_all_traces.png');
+system( 'montage ' . $samples_folder . '/*.png examples/montage_all_traces.png' );
 
